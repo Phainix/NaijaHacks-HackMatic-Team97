840b3e8534b3d23577d0263d508fc806748f6e72
Adekogbe Faith <28295747+Phainix@users.noreply.github.com>
2019-12-13 06:06:18 +0100

    Home

--- EdMatic/app/src/main/java/com/hackmatic/edmatic/ui/home/HomeFragment.java
+++ EdMatic/app/src/main/java/com/hackmatic/edmatic/ui/home/HomeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -13,6 +14,10 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.hackmatic.edmatic.R;
+import com.hackmatic.edmatic.data.Activity;
+import com.hackmatic.edmatic.data.Challenge;
+
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
@@ -23,13 +28,32 @@ public class HomeFragment extends Fragment {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+
+        List<Challenge> challenges = homeViewModel.getUpcomingChallenges();
+        LinearLayout upcomingLayout = (LinearLayout) root.findViewById(R.id.upcoming_list);
+        for (int i = 0; i < challenges.size(); i++) {
+            View upcomingItem = inflater.inflate(R.layout.upcoming_list_item, null);
+            TextView name = (TextView) upcomingItem.findViewById(R.id.name);
+            name.setText(challenges.get(i).getmName());
+            TextView time = (TextView) upcomingItem.findViewById(R.id.time);
+            time.setText(challenges.get(i).getmTime());
+            TextView team = (TextView) upcomingItem.findViewById(R.id.team_name);
+            team.setText(challenges.get(i).getmTeam());
+            upcomingLayout.addView(upcomingItem);
+        }
+
+        List<Activity> activities = homeViewModel.getActivities();
+        LinearLayout activitiesLayout = (LinearLayout) root.findViewById(R.id.activities);
+        for (int i = 0; i < activities.size(); i++) {
+            View activity = inflater.inflate(R.layout.activity_list_item, null);
+            TextView name = (TextView) activity.findViewById(R.id.name);
+            name.setText(activities.get(i).getmName());
+            TextView time = (TextView) activity.findViewById(R.id.time);
+            time.setText(activities.get(i).getmTime());
+            activitiesLayout.addView(activity);
+        }
+
         return root;
     }
 }
--- EdMatic/app/src/main/java/com/hackmatic/edmatic/ui/home/HomeViewModel.java
+++ EdMatic/app/src/main/java/com/hackmatic/edmatic/ui/home/HomeViewModel.java
@@ -4,16 +4,51 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.hackmatic.edmatic.data.Activity;
+import com.hackmatic.edmatic.data.Challenge;
+
+import java.util.ArrayList;
+import java.util.BitSet;
+import java.util.List;
+
 public class HomeViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
+    private List<Challenge> mNotes = new ArrayList<>();
 
     public HomeViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+
     }
 
     public LiveData<String> getText() {
         return mText;
+    }
+
+    public List<Challenge> getUpcomingChallenges() {
+        Challenge challenge1 = new Challenge("Math Masters (Phase 4)", "8:30 AM - 02:00 PM", "Math Rockers");
+        Challenge challenge2 = new Challenge("Chemistry National Championship", "8:30 AM - 02:00 PM", "Chemistry Rockers");
+        Challenge challenge3 = new Challenge("Physics 2019 Juniors Qualification", "8:30 AM - 02:00 PM", "Physicians");
+
+        List<Challenge> challenges =  new ArrayList<Challenge>();
+        challenges.add(challenge1);
+        challenges.add(challenge2);
+        challenges.add(challenge3);
+        return challenges;
+    }
+
+    public List<Activity> getActivities() {
+        Activity activity1 = new Activity("Issued Challenge", " 8:40 AMM");
+        Activity activity2 = new Activity("Won Math Masters Phase (2)", "9:00 AM - 9:50 AM");
+        Activity activity3 = new Activity("Accepted Physics Challenge", "10:00 AM ");
+        Activity activity4 = new Activity("Joined Lagos Math Gurus", "11:00 AM");
+        Activity activity5 = new Activity("Created Physics Challenge", "02:00 AM ");
+
+        List<Activity> activities =  new ArrayList<Activity>();
+        activities.add(activity1);
+        activities.add(activity2);
+        activities.add(activity3);
+        activities.add(activity4);
+        activities.add(activity5);
+        return activities;
     }
 }
